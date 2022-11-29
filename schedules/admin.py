c5b17b8d098170ec0c8bddf3e678c219a8af22a4
kangdh208 <kangdh208@gmail.com>
2022-11-29 17:52:23 +0900

    UPDATE : 스케쥴러 기능 변경/추가

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from schedules.models import Schedule
-# Register your models here.
+from schedules import models
 
-admin.site.register(Schedule)
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    model = models.Event
+    list_display = [
+        "id",
+        "title",
+        "user",
+        "is_active",
+        "is_deleted",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["is_active", "is_deleted"]
+    search_fields = ["title"]
+
+
+@admin.register(models.EventMember)
+class EventMemberAdmin(admin.ModelAdmin):
+    model = models.EventMember
+    list_display = ["id", "event", "user", "created_at", "updated_at"]
+    list_filter = ["event"]
