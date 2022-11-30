4bf913202a03a9d5d06429cbc12945dfcae11d1f
mgpo-98 <csi4613@naver.com>
2022-11-30 17:02:36 +0900

    ADD : detail페이지

--- reviews/models.py
+++ reviews/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 from django.db import models
 from django.conf import settings
-from imagekit.models import  ProcessedImageField
+from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
 # 이미지
-
-
-
-
-
-
 
 
 # Create your models here.
@@ -19,20 +13,18 @@ class Review(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-   
-    image = ProcessedImageField(upload_to='reviews/images/', blank=True,
-                            processors=[ResizeToFill(1200, 960)],
-                            format='JPEG',
-                            options={'quality': 80})
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = ProcessedImageField(
+        upload_to="reviews/images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 80},
+    )
 
 
-
-       
 class Comment(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
-
-
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,27 +1,28 @@
 from django.urls import path
 from . import views
+
 # Create your views here.
 
-app_name ='reviews'
+app_name = "reviews"
 urlpatterns = [
     # 멍스타그렘
-    path('', views.index, name='index'),
-
-    # 리뷰작성 
-    path('create/', views.create, name='create'),
-    
-    #리뷰 수정
-    path('<int:pk>/update/', views.update, name='update'),
-
+    path("", views.index, name="index"),
+    # 리뷰작성
+    path("create/", views.create, name="create"),
+    path("<int:pk>/", views.detail, name="detail"),
+    # 리뷰 수정
+    path("<int:pk>/update/", views.update, name="update"),
+    # 리뷰 수정
+    path("<int:pk>/update/", views.update, name="update"),
     # 리뷰 삭제
-    path('<int:pk>/delete/', views.delete, name='delete'),
-
+    path("<int:pk>/delete/", views.delete, name="delete"),
     # 댓글 작성
-    path('<int:pk>/comments/', views.comment_create, name='comment_create'),
-    
+    path("<int:pk>/comments/", views.comment_create, name="comment_create"),
     # 댓글 삭제
-    path('<int:pk>/comments/<int:comment_pk>/delete/', views.comment_delete, name ='comment_delete'),
-
+    path(
+        "<int:pk>/comments/<int:comment_pk>/delete/",
+        views.comment_delete,
+        name="comment_delete",
+    ),
     # 해시태그
- 
 ]
