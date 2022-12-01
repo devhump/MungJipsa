421c4aa2a6f288ccd5081c4342942d823d311436
Bonbon315 <n202u123@gmail.com>
2022-12-01 13:39:45 +0900

    feat: hospital 페이지 초안 마무리

--- info/views.py
+++ info/views.py
@@ -1,28 +1,56 @@
 from django.shortcuts import render, redirect
 from .models import Hospital, Deserted
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def hospital(request):
     hospitals = Hospital.objects.all()
+    search = request.GET.get("search")
+    areaSearch = request.GET.get("areaSearch")
+    if not areaSearch:
+        areaSearch = "전국"
+    if areaSearch == "전국":
+        areaSearch = ""
+
+    if search:
+        hospitals = hospitals.filter(
+            Q(name__icontains=search) | Q(address__icontains=search)
+        )
+        hospitals = hospitals.filter(Q(address__icontains=areaSearch))
+    else:
+        search = ""
+        hospitals = hospitals.filter(Q(address__icontains=areaSearch))
     paginator = Paginator(hospitals, 12)
     page = request.GET.get("page")
     posts = paginator.get_page(page)
 
-    leftIndex = int(page) - 3
-    if leftIndex < 1:
-        leftIndex = 1
-
-    rightIndex = int(page) + 3
-    if rightIndex > paginator.num_pages:
-        rightIndex = paginator.num_pages
-
-    custom_range = range(leftIndex, rightIndex + 1)
-
     context = {
+        "searchval": search,
+        "areaval": areaSearch,
         "hospitals": hospitals,
         "posts": posts,
         "paginator": paginator,
-        "custom_range": custom_range,
     }
+
+    if page:
+        leftIndex = int(page) - 3
+        if leftIndex < 1:
+            leftIndex = 1
+
+        rightIndex = int(page) + 3
+        if rightIndex > paginator.num_pages:
+            rightIndex = paginator.num_pages
+
+        custom_range = range(leftIndex, rightIndex + 1)
+
+        context = {
+            "searchval": search,
+            "areaval": areaSearch,
+            "hospitals": hospitals,
+            "posts": posts,
+            "paginator": paginator,
+            "custom_range": custom_range,
+        }
+
     return render(request, "info/hospital.html", context)
