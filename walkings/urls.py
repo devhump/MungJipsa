d1ee68d374bb1419bfb3765cd72bf224ab548102
devhum7p <devhum7p@gmail.com>
2022-12-01 14:41:22 +0900

    TEST : walkings detail 페이지

--- walkings/urls.py
+++ walkings/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("test/", views.test, name="test"),
     path("create/<int:park_pk>", views.create, name="create"),
+    path("detail/<int:dogroup_pk>", views.detail, name="detail"),
 ]
--- walkings/views.py
+++ walkings/views.py
@@ -4,6 +4,8 @@ from .forms import MylocationForm, DogroupForm
 import json
 from pprint import pprint
 import requests
+from django.http import JsonResponse
+
 
 # Create your views here.
 def index(request):
@@ -21,6 +23,9 @@ def test(request):
 
     parks = Park.objects.filter(address__icontains="동작구")
 
+    dogroups = Dogroup.objects.all()
+    user_dogs = Dog.objects.filter(user=request.user)
+
     park_info = []
     for park in parks:
 
@@ -36,6 +41,8 @@ def test(request):
     parkJson = json.dumps(park_info)
     context = {
         "parkJson": parkJson,
+        "dogroups": dogroups,
+        "user_dogs": user_dogs,
     }
 
     return render(request, "walkings/test.html", context)
@@ -59,28 +66,37 @@ def create(request, park_pk):
             dogroup.dog = dog
             dogroup.park = park
             dogroup.save()
-            return redirect("walkings:test")
-    else:
-        dogroup_form = DogroupForm()
 
-    dogroup = Dogroup.objects.all()
+    # dogroup = Dogroup.objects.all()
 
-    dogroup_data = []
+    # dogroup_data = []
 
-    for dogr in dogroup:
+    # for dogr in dogroup:
 
-        temp = {
-            "dogroup_pk": dogr.pk,
-            "dogroup_date": dogr.date,
-            "dogroup_title": dogr.membercnt,
-            "dogroup_dog_pk": dogr.dog_id,
-            "dogroup_park_pk": dogr.park_id,
-            "dogroup_park_pk": dogr.user_id,
-        }
+    #     temp = {
+    #         "dogroup_pk": dogr.pk,
+    #         "dogroup_date": dogr.date,
+    #         "dogroup_title": dogr.membercnt,
+    #         "dogroup_dog_pk": dogr.dog.pk,
+    #         "dogroup_park_pk": dogr.park.pk,
+    #         "dogroup_park_pk": dogr.user.pk,
+    #     }
 
-        dogroup_data.append(temp)
+    #     dogroup_data.append(temp)
+    # print(dogroup_data)
+    # context = {
+    #     "dogroup_data": dogroup_data,
+    # }
+
+    # return JsonResponse(context)
+    return redirect("walkings:test")
+
+
+def detail(request, dogroup_pk):
+
+    dogroup = Dogroup.objects.get(pk=dogroup_pk)
+
     context = {
-        "dogroup_data": dogroup_data,
+        "dogroup": dogroup,
     }
-
-    return render(request, "walkings/create.html", context)
+    return render(request, "walkings/detail.html", context)
