58bfc124b838b3d8e50f7a6d9d11dd4ae54f57d3
devhum7p <devhum7p@gmail.com>
2022-12-09 16:25:16 +0900

    FIX: 배포 오류 수정

--- namung/settings.py
+++ namung/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 if DEBUG:
